7cbe5ab68d2ccccf2c5e6b998572eff738059c9f
Dalia Barranco <DALIABARRANCO16@GMAIL.COM>
2025-12-09 17:01:10 -0500

    Completed week 3 deliverables.Worked with props and state.

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,19 @@ import './App.css'
 import TodoList from './TodoList'
 //Importing TodoForm
 import TodoForm from "./TodoForm";
+//Importing useState hook
+import { useState } from "react";
 
 function App() {
+  //State value that will hold a new todo
+  const [newTodo, setNewTodo] = useState("initialValue");
   return (
     <div>
       <h1>My Todos</h1>
       {/* Adding instance of the TodoForm */}
       <TodoForm />
+      {/*paragraph */}
+      <p>{newTodo}</p>
       {/* Adding instance of the TodoList */}
       <TodoList />
     </div>
