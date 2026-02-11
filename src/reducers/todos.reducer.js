8b168bc09df94739f46cddba9212621dc450e76e
Dalia Barranco <DALIABARRANCO16@GMAIL.COM>
2026-02-11 18:40:26 -0500

    Created reducer function

--- src/reducers/todos.reducer.js
+++ src/reducers/todos.reducer.js
@@ -1,3 +1,33 @@
+export const actions ={
+    fetchTodos: 'fetchTodos',
+    loadTodos: 'loadTodos',
+
+    setLoadError:'setLoadError',
+
+    startRequest: 'startRequest',
+    addTodo: 'addTodo',
+    endRequest: 'endRequest',
+
+    updateTodo: 'updateTodo',
+    completeTodo: 'completeTodo',
+
+    revertTodo: 'revertTodo',
+
+    clearError: 'clearError',
+};
+
+function reducer (state = initialState, action){
+    switch(action.type){
+        case actions.fetchTodos:
+            return {
+                ...state,
+            };
+        case actions.loadTodos:
+            return{
+                ...state,
+            };
+    }
+}
 export const initialState = {
     todoList: [],
     isLoading: false,
