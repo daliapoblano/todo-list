c57eb9c18c2963ae9b3f0cf2d036edc94a912e50
Dalia Barranco <DALIABARRANCO16@GMAIL.COM>
2026-03-05 21:52:38 -0500

    Created 2 navlink components and gave them a className props

--- src/shared/Header.jsx
+++ src/shared/Header.jsx
@@ -1,9 +1,25 @@
+import { NavLink } from "react-router-dom";
 import styles from "./Header.module.css";
 
 function Header({ title }) {
     return (
         <header>
             <h1>{title}</h1>
+
+            <nav>
+                <NavLink
+                    to="/"
+                    className={({ isActive }) => (isActive ? "active" : "inactive")}
+                >
+                    Home 
+                </NavLink>
+                <NavLink
+                    to="/about"
+                    className={({ isActive }) => (isActive ? "active" : "inactive")}
+                >
+                    About
+                </NavLink>
+            </nav>
         </header>
     );
 }
