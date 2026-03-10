c0b999a848e8b320b34d56b3c94e0d64fbb1fdb3
Dalia Barranco <DALIABARRANCO16@GMAIL.COM>
2026-03-09 22:03:50 -0400

    Added content to not found file so users can navigate back to the home page

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+function NotFound() {
+    return(
+        <div>
+            <h2>Page Not Found</h2>
+            <p>Sorry, the page you are looking for does not exist.</p>
+            <Link to="/">Go back home</Link>
+        </div>
+    );
+}
+
+export default NotFound;
