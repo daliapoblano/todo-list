d7d844e65ac2deca30e338fac2931399602c015f
Dalia Barranco <DALIABARRANCO16@GMAIL.COM>
2026-03-09 21:47:01 -0400

    Added content to About Page

---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,11 @@
+function AboutPage() {
+    return(
+        <div>
+            <h2>About</h2>
+            <p>Author Information: Hello! My name is Dalia Poblano and I am a Code the Dream (CTD) React course student.</p>
+            <p>This is a simple todo list app built with React.</p>
+        </div>
+    );
+}
+
+export default AboutPage;
